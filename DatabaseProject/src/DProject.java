fd50debad853c92b62c07e8a76aa570305df22e8
skippy <skippy@skippy-PC>
2017-11-13 17:00:36 -0500

    testing from home

--- DatabaseProject/src/DProject.java
+++ DatabaseProject/src/DProject.java
@@ -3,7 +3,7 @@ public class DProject {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("testing 3");
+		System.out.println("testing 4");
 
 	}
 
