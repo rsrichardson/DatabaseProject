ace3261b0705dcd09bbe6f8118c3246f79f1aaa1
Skippy's laptop <rsrichardson@oakland.edu>
2017-11-15 17:04:57 -0500

    button testing

--- DatabaseProject/src/DProject.java
+++ DatabaseProject/src/DProject.java
@@ -8,16 +8,24 @@ public class DProject extends JFrame
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	JButton next;
-	
+	JButton next, SB;
+	Container contents;
+	Panel panel1, panel2;
 	
 	public DProject()
 	{
-		Container contents = getContentPane();
+		contents = getContentPane();
 		contents.setLayout(new FlowLayout());
 		next = new JButton("next");
+		SB = new JButton("here");
 		
-		contents.add(next);
+		panel1 = new Panel();
+		panel2 = new Panel();
+		
+		panel1.add(next);
+		contents.add(panel1);
+		
+		panel2.add(SB);
 		
 		ButtonHandler bh = new ButtonHandler();
 		next.addActionListener(bh);
@@ -32,8 +40,10 @@ public class DProject extends JFrame
 		{
 			if(a.getSource() == next)
 			{
-				Second S = new Second();
-				S.setVisible(true);
+				contents.remove(panel1);
+				contents.add(panel2);
+				setVisible(false);
+				setVisible(true);
 			}
 		}
 		
